65071e1554e6c09a9ecb259ea98dbb24a43b6dfe
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-16 09:52:51 -0600

    safe fql contents

--- library/class.fql.inc.php
+++ library/class.fql.inc.php
@@ -3,16 +3,16 @@ namespace library;
 
 class fql
 {
-    /**
-     * Read the SQL from file and rename the oc_ prefix.
-     */
     public function read($sql_filename="")
     {
-        $file = __OCUNIT_ROOT__."/sql/".$sql_filename;
-        assert(file_exists($file));
-       
-        $sql = file_get_contents($file);
-        $sql = str_replace("oc_", DB_PREFIX, $sql);
+        $sql = "";
+
+        $file = __OCUNIT_ROOT__."/sql/".basename($sql_filename);
+        if(is_file($file))
+        {
+            $sql = file_get_contents($file);
+            $sql = str_replace("oc_", DB_PREFIX, $sql);
+        }
 
         return $sql;
     }

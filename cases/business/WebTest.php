604eb5902cbbf510269007fb758a3ca9a0fe810d
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-16 06:48:07 -0600

    Force a message for stronger password

---
+++ cases/business/WebTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace cases\business;
+
+use \PHPUnit\Framework\TestCase;
+
+class WebTest extends TestCase
+{
+    public function testForceStrongPassword()
+    {
+        $this->markTestIncomplete("Show a message to choose strong password.");
+    }
+}

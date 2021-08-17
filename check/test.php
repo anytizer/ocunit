ae804244485d1ab13d71448e49c40e368eb8bcdf
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-16 20:24:17 -0600

    Incomplete adaptation from beyondit/opencart-test-suite

--- check/test.php
+++ check/test.php
@@ -4,17 +4,21 @@ require_once "OpenCartTest.php";
 
 use Opencart\Catalog\Controller\Startup\Setting as Setting;
 use \Opencart\System\Engine\Registry as Registry;
+use \Opencart\System\Engine\Event as Event;
 
 class Test extends OpenCartTest
 {
     public function testIndexLoaded()
     {
         $registry = new Registry();
-        $route = "common/dashboard";
-        $registry->set('controller', $route);
 
-        $event = new \Opencart\System\Engine\Event($registry);
+        #$route = "common/dashboard";
+        #$registry->set('controller', $route);
+
+        $event = new Event($registry);
         $registry->set('event', $event);
+
         print_r($registry);
+        print_r($event);
     }
 }

cf9de4e697a1305e36c9c2d2a4f43d1ebc147b80
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-18 20:57:42 -0600

    using double quotes

--- cases/general/SettingsTest.php
+++ cases/general/SettingsTest.php
@@ -8,34 +8,34 @@ class SettingsTest extends TestCase
 {
 	public function testDatabaseConstantsDefined()
     {
-        $this->assertTrue(defined('DB_DRIVER'));        
-        $this->assertTrue(defined('DB_HOSTNAME'));
-        $this->assertTrue(defined('DB_USERNAME'));
-        $this->assertTrue(defined('DB_PASSWORD'));
-        $this->assertTrue(defined('DB_DATABASE'));
-        $this->assertTrue(defined('DB_PORT'));
-        $this->assertTrue(defined('DB_PREFIX'));
+        $this->assertTrue(defined("DB_DRIVER"));
+        $this->assertTrue(defined("DB_HOSTNAME"));
+        $this->assertTrue(defined("DB_USERNAME"));
+        $this->assertTrue(defined("DB_PASSWORD"));
+        $this->assertTrue(defined("DB_DATABASE"));
+        $this->assertTrue(defined("DB_PORT"));
+        $this->assertTrue(defined("DB_PREFIX"));
     }
 
     public function testOtherSettingsDefinedInFrontend()
     {
-        $this->assertTrue(defined('APPLICATION'));
-        $this->assertTrue(defined('HTTP_SERVER'));
+        $this->assertTrue(defined("APPLICATION"));
+        $this->assertTrue(defined("HTTP_SERVER"));
 
-        $this->assertTrue(defined('DIR_OPENCART'));
-        $this->assertTrue(defined('DIR_APPLICATION'));
-        $this->assertTrue(defined('DIR_EXTENSION'));
-        $this->assertTrue(defined('DIR_IMAGE'));
-        $this->assertTrue(defined('DIR_SYSTEM'));
-        $this->assertTrue(defined('DIR_STORAGE'));        
-        $this->assertTrue(defined('DIR_LANGUAGE'));
-        $this->assertTrue(defined('DIR_TEMPLATE'));
-        $this->assertTrue(defined('DIR_CONFIG'));
-        $this->assertTrue(defined('DIR_CACHE'));
-        $this->assertTrue(defined('DIR_DOWNLOAD'));
-        $this->assertTrue(defined('DIR_LOGS'));
-        $this->assertTrue(defined('DIR_SESSION'));
-        $this->assertTrue(defined('DIR_UPLOAD'));
+        $this->assertTrue(defined("DIR_OPENCART"));
+        $this->assertTrue(defined("DIR_APPLICATION"));
+        $this->assertTrue(defined("DIR_EXTENSION"));
+        $this->assertTrue(defined("DIR_IMAGE"));
+        $this->assertTrue(defined("DIR_SYSTEM"));
+        $this->assertTrue(defined("DIR_STORAGE"));
+        $this->assertTrue(defined("DIR_LANGUAGE"));
+        $this->assertTrue(defined("DIR_TEMPLATE"));
+        $this->assertTrue(defined("DIR_CONFIG"));
+        $this->assertTrue(defined("DIR_CACHE"));
+        $this->assertTrue(defined("DIR_DOWNLOAD"));
+        $this->assertTrue(defined("DIR_LOGS"));
+        $this->assertTrue(defined("DIR_SESSION"));
+        $this->assertTrue(defined("DIR_UPLOAD"));
     }
 
     public function testStorageAreaIsOutOfUpload()

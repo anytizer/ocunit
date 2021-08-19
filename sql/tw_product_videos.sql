851d993c242f64754416d92dea756ee67f9a5a62
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-19 09:05:59 -0600

    videos for products

---
+++ sql/tw_product_videos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `tw_product_videos` (
+  `video_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) unsigned NOT NULL,
+  `video_code` varchar(255) NOT NULL,
+  `video_link` varchar(255) NOT NULL,
+  PRIMARY KEY (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

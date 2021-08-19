a8a7feaecfca215dcf13eba0e62cb3c54afef718
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-18 20:45:03 -0600

    locate where

--- cases/business/WebTest.php
+++ cases/business/WebTest.php
@@ -5,8 +5,10 @@ use \PHPUnit\Framework\TestCase;
 
 class WebTest extends TestCase
 {
-    public function testForceStrongPassword()
+    public function testForceStrongPasswordInWebPages()
     {
+        // user registration
+        // password change
         $this->markTestIncomplete("Show a message to choose strong password.");
     }
 }

26ba689d71d775e3153b3406311e8f95b3c2fdc8
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-20 21:35:35 -0600

    testing method

--- cases/business/WebTest.php
+++ cases/business/WebTest.php
@@ -10,6 +10,7 @@ class WebTest extends TestCase
         // user registration page
         // password change page
         // password reset link email
+        // modify the default theme page to insert extra message
         $this->markTestIncomplete("Show a message to choose strong password.");
     }
 }

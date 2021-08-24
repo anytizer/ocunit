3e8c10bcffb48a6fefb9465ff005b318aad6f36f
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-23 22:10:51 -0600

    Cannot load language

--- cases/core/LanguageTest.php
+++ cases/core/LanguageTest.php
@@ -8,23 +8,53 @@ require_once DIR_OPENCART."system/library/language.php";
 
 class LanguageTest extends TestCase
 {
+    public function setUp(): void
+    {
+        // $language->clear();
+    }
+
     public function testLanguageLoaded()
     {
-        $namespace = "Opencart\\Catalog\\Controller\\Account";
-        $prefix = "text_";
+        $this->markAsRisky();
+
+        // $file = $this->directory . $code . '/' . $filename . '.php';
+        // $file = $this->path[$namespace] . $code . substr($filename, strlen($namespace)) . '.php';
+
+        // language files attempted to load in home page
+        //
+        // catalog/language/en-gb/en-gb.php
+        // catalog/language/en-gb/en-gb.php
+        // extension/opencart/catalog/language/en-gb/module/category.php
+        // catalog/language/en-gb/common/footer.php
+        // catalog/language/en-gb/common/header.php
+        // catalog/language/en-gb/common/language.php
+        // catalog/language/en-gb/common/currency.php
+        // catalog/language/en-gb/common/search.php
+        // catalog/language/en-gb/common/cart.php
+        // extension/opencart/catalog/language/en-gb/total/sub_total.php
+        // extension/opencart/catalog/language/en-gb/total/credit.php
+        // extension/opencart/catalog/language/en-gb/total/total.php
+        // catalog/language/en-gb/common/menu.php
+
+        $namespace = "catalog"; // "Opencart\\Catalog"; // "Opencart\\Catalog\\Controller\\Account"
         $code = "en-gb";
-        $filename = DIR_OPENCART."catalog/language/en-gb/account/login.php";
+        $directory = DIR_OPENCART."catalog/language/";
+        $filename = "account/account";
+        $prefix = "text_";
 
         $language = new Language($code);
 
-        $language->addPath($namespace);
-        $data_cart = $language->load($filename, $prefix, $code);
-        $text_cart = $language->get("text_cart");
+        $language->addPath($namespace, "");
+        $language->addPath($namespace, $directory);
+        $data = $language->load($filename, $prefix, $code);
+        $text = $language->get("text_account");
 
-        print_r($language);
-        print_r($data_cart);
-        print_r($text_cart);
+        // @todo Remove this output
+        #echo "\r\nOutput from: ", __CLASS__, "::", __METHOD__, "()\r\n";
+        #print_r($language);
+        #print_r($data);
+        #print_r($text);
 
-        $this->assertEquals("View Cart", $text_cart);
+        $this->assertEquals("Account", $text, "Failed reading the language data.");
     }
 }

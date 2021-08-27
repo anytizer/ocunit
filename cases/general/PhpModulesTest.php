f99dfb6a8ddc51b9f743eb14b3fc0301f9a8303d
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-26 20:35:09 -0600

    code merge

--- cases/general/PhpModulesTest.php
+++ cases/general/PhpModulesTest.php
@@ -21,4 +21,23 @@ class PhpModulesTest extends TestCase
         $this->assertTrue(function_exists("random_bytes"), "Missing PHP function: random_bytes");
         $this->assertTrue(function_exists("openssl_random_pseudo_bytes"), "Missing PHP extension: OpenSSL");
     }
+
+    public function testZendAssertions()
+    {
+        $assertion = ini_get("zend.assertions");
+        $this->assertEquals(-1, $assertion, "Production value of Zend Assertion should be -1.");
+    }
+
+    /**
+     * Standard PHP Tests
+     */
+    public function testAllIncludedPathsExist()
+    {
+        $paths = explode(PATH_SEPARATOR, ini_get("include_path"));
+
+        foreach($paths as $path)
+        {
+            $this->assertTrue(is_dir($path), "Include path does not exist: {$path}.");
+        }
+    }
 }

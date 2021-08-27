5a85f03d317a7acb60c7b9fcbe754ca6d040cf45
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-26 22:06:35 -0600

    columns reordered

--- sql/tw_login_failures.sql
+++ sql/tw_login_failures.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `tw_login_failures` (
-  `login_failure_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `login_username` VARCHAR(255) NOT NULL,
-  `login_date` DATETIME NOT NULL,
-  `login_ip` VARCHAR(255) NOT NULL,
-  `login_broser` VARCHAR(255) NOT NULL,
+  `login_failure_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `login_datetime` datetime NOT NULL,
+  `login_username` varchar(255) NOT NULL DEFAULT '',
+  `login_ip` varchar(255) NOT NULL DEFAULT '',
+  `login_browser` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`login_failure_id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

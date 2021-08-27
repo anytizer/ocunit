2272e886cad77b36163b78f8c0cc57629bd8f093
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-27 12:53:48 -0600

    split

--- library/class.catalog.inc.php
+++ library/class.catalog.inc.php
@@ -45,45 +45,6 @@ class catalog
         return $html;
 	}
 
-	public function browse_categories(): string
-	{
-		/**
-		 * Used electronics, wires and parts
-		 * @see http://localhost/opencart/upload/index.php?route=product/category&language=en-gb&path=66_63
-		 */
-		$_GET = [
-			"route" => "product/category",
-			"language" => "en-gb",
-			"path" => "66_63",
-		];
-        $_POST = [];
-		$relay = new relay();
-		$relay->headers([
-			"X-Protection-Token" => "",
-		]);
-		$this->html = $relay->fetch(HTTP_SERVER."index.php");
-	}
-
-	public function browse_product(): string
-	{
-		/**
-		 * Micro SD Card 32GB
-		 * @see http://localhost/opencart/upload/index.php?route=product/product&language=en-gb&path=66_63&product_id=82
-		 */
-		$_GET = [
-			"route" => "product/product",
-			"language" => "en-gb",
-			"path" => "66_63",
-			"product_id" => 82,
-		];
-        $_POST = [];
-		$relay = new relay();
-		$relay->headers([
-			"X-Protection-Token" => "",
-		]);
-		$this->html = $relay->fetch(HTTP_SERVER."index.php");
-	}
-
     /**
      * Open an arbitrary simple page for testing its HTML in GET mode using full url
      *
@@ -127,7 +88,7 @@ class catalog
 
     /**
      * @see // index.php?route=account/login|login&language=en-gb&login_token=5654914f48eccb41c6eb08fec3
-     * @return bool|string
+     * @return string
      */
     public function login_advanced(): string
     {
@@ -135,6 +96,7 @@ class catalog
 			"route" => "account/login|login",
 			"language" => "en-gb",
 
+            // @todo Generate login token
 			"login_token" => "5654914f48eccb41c6eb08fec3",
 		];
 
@@ -149,6 +111,21 @@ class catalog
         ]);
 		$html = $relay->fetch(HTTP_SERVER."index.php");
 
+        return $html;
+    }
+
+    public function lookup($post_query)
+    {
+        $page = "index.php";
+        $_GET = $post_query["get"];
+        $_POST = $post_query["post"];
+
+        $relay = new relay();
+        $relay->headers([
+            "X-Protection-Token" => "",
+        ]);
+
+        $html = $relay->fetch(HTTP_SERVER.$page);
         return $html;
     }
 }

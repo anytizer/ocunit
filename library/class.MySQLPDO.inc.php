b9599ba24f201f1ef915a7154c9e53866cb0eac5
Bimal Poudel <anytizer@users.noreply.github.com>
2021-08-27 20:41:41 -0600

    Using double quotes

--- library/class.MySQLPDO.inc.php
+++ library/class.MySQLPDO.inc.php
@@ -15,15 +15,15 @@ class MySQLPDO
 
     public function __construct()
     {
-        $dsn = 'mysql:host='.DB_HOSTNAME.';dbname='.DB_DATABASE.';port=3306;';
+        $dsn = "mysql:host=".DB_HOSTNAME.";dbname=".DB_DATABASE.";port=3306;";
         $this->connection = new PDO(
             $dsn,
             DB_USERNAME,
             DB_PASSWORD,
             array(
-                // PDO::MYSQL_ATTR_SSL_KEY  => '/path/to/client-key.pem',
-                // PDO::MYSQL_ATTR_SSL_CERT => '/path/to/client-cert.pem',
-                // PDO::MYSQL_ATTR_SSL_CA   => '/path/to/ca-cert.pem',
+                // PDO::MYSQL_ATTR_SSL_KEY  => "/path/to/client-key.pem",
+                // PDO::MYSQL_ATTR_SSL_CERT => "/path/to/client-cert.pem",
+                // PDO::MYSQL_ATTR_SSL_CA   => "/path/to/ca-cert.pem",
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             )
         );
@@ -32,7 +32,7 @@ class MySQLPDO
     /**
      * Queries that return the data - like: select, count, show
      */
-    public function query($sql='', $data=[])
+    public function query($sql="", $data=[])
     {
         $statement = $this->connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY,]);
         $statement->execute($data);
@@ -47,7 +47,7 @@ class MySQLPDO
     /**
      * Fire-only queries - like: insert, update, delete, replace
      */
-    public function raw($sql='', $data=[]): void
+    public function raw($sql="", $data=[]): void
     {
         $statement = $this->connection->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY,]);
         $statement->execute($data);

eb487a299029fd1e7fe6ac23ab263f4a38e1ade1
Bimal Poudel <anytizer@users.noreply.github.com>
2022-06-01 02:48:11 -0600

    general

--- admin/bootstrap.php
+++ admin/bootstrap.php
@@ -9,6 +9,6 @@ require_once dirname(__FILE__) . "/../library/oc.php";
 
 $oc = new oc();
 $oc->must_include(realpath($configurations["opencart"]["admin"]), "config.php");
-$oc->must_define("HTTP_CATALOG"); // admin defines frontend
+$oc->must_define("HTTP_CATALOG"); // admin defines where the frontend is
 
 require_once("../continuation.inc.php");

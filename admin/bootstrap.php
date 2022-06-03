ac40a393a754cd344e8a2552301b0cbc26934d75
Bimal Poudel <anytizer@users.noreply.github.com>
2022-06-02 21:03:36 -0600

    IDE silencer

--- admin/bootstrap.php
+++ admin/bootstrap.php
@@ -4,11 +4,12 @@ namespace ocunit\admin;
 
 use ocunit\library\oc as oc;
 
+$configurations = [];
 require_once dirname(__FILE__) . "/../config.php";
 require_once dirname(__FILE__) . "/../library/oc.php";
 
 $oc = new oc();
 $oc->must_include(realpath($configurations["opencart"]["admin"]), "config.php");
-$oc->must_define("HTTP_CATALOG"); // admin defines where the frontend is
+$oc->must_define("HTTP_CATALOG"); // admin should define where the store is
 
 require_once("../continuation.inc.php");

5caf8bdcf7a3129235afc8053266f2e022e525d0
Bimal Poudel <anytizer@users.noreply.github.com>
2022-06-03 19:25:59 -0600

    sqls change

--- sql/oc_setting.sql
+++ sql/oc_setting.sql
@@ -6,4 +6,7 @@ UPDATE `oc_setting` SET `value` = '20' WHERE `key` = 'config_pagination';
 UPDATE `oc_setting` SET `value` = '100' WHERE `key` = 'config_pagination_admin';
 UPDATE `oc_setting` SET `value` = 'America/Edmonton' WHERE `key` = 'config_timezone';
 
+-- Display taxes separately
+UPDATE `oc_setting` SET `value`='0' WHERE `code`='config' AND `key`='config_tax';
+
 UPDATE `oc_product` SET sku=model WHERE sku='';
